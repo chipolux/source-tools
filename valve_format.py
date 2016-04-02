b3677dfa776420f092c3f8479c2f72bfc5353a72
NaKyle <nakyle.wright@gmail.com>
2016-04-02 00:31:30 -0500

    Add more parsing capabilities.

--- valve_format.py
+++ valve_format.py
@@ -1,17 +1,62 @@
-# import re
-
-
 class Branch:
-    def __init__(self, name):
+    def __init__(self, name, parent=None, is_file=False):
         self.name = name
-        self._leaves = []
-        self._branches = []
+        self.parent = parent
+        self.is_file = is_file
+        self.leaves = []
+        self.branches = []
 
-    def add_leaf(self, key, value):
-        self._leaves.append((key, value))
+        if self.parent is not None:
+            self.parent.add_branch(self)
 
     def add_branch(self, branch):
-        self._branches.append(branch)
+        if branch.parent != self:
+            branch.parent = self
+
+        if branch not in self.branches:
+            self.branches.append(branch)
+
+    def add_leaf(self, key, value):
+        leaf = (key, value)
+        if leaf not in self.leaves:
+            self.leaves.append(leaf)
+
+    def branches_by_name(self, name):
+        return list(filter(lambda x: x.name == name, self.branches))
+
+    def leaves_by_key(self, key):
+        return list(filter(lambda x: x[0] == key, self.leaves))
+
+    def serialize(self):
+        if self.is_file:
+            for leaf in self.leaves:
+                yield '"{}"\t"{}"'.format(*leaf)
+            for branch in self.branches:
+                for line in branch.serialize():
+                    yield line
+        else:
+            yield '"{}"\n'.format(self.name)
+            yield '{\n'
+            for leaf in self.leaves:
+                yield '\t"{}"\t"{}"\n'.format(*leaf)
+            for branch in self.branches:
+                for line in branch.serialize():
+                    yield '\t' + line
+            yield '}\n'
+
+    def __getattr__(self, name):
+        branches = self.branches_by_name(name)
+        if len(branches) > 1:
+            raise AttributeError('Multiple branches named "{}"!'.format(name))
+        elif len(branches) < 1:
+            raise AttributeError('No branches named "{}"!'.format(name))
+        else:
+            return branches[0]
+
+    def __repr__(self):
+        return '<Branch(name={}, leaves={}, branches={})>'.format(
+            self.name, len(self.leaves), len(self.branches)
+        )
 
 
 def parse_line(line):
@@ -32,6 +77,7 @@ def parse_line(line):
         line = line[end_quote + 1:]
     else:
         # Unquoted string key
+        # TODO: handle comment starting without a space after value
         try:
             key, line = line.split(maxsplit=1)
         except ValueError:
@@ -48,6 +94,7 @@ def parse_line(line):
         value = line[0:line.find('"')]
     else:
         # Unquoted string value
+        # TODO: handle comment starting without a space after value
         value = line.split(maxsplit=1)[0]
 
     # Anything after the value can be discarded
@@ -57,11 +104,27 @@ def parse_line(line):
 with open('gameinfo.txt', 'r') as f:
     lines = f.readlines()
 
+# Reduce file down to core data structure
 parsed_lines = []
 for line in lines:
     line = parse_line(line)
     if line is not None:
         parsed_lines.append(line)
 
+# Build branches
+root_branch = Branch(name='gameinfo.txt', is_file=True)
+current_branch = root_branch
+for line in parsed_lines:
+    if isinstance(line, tuple):
+        current_branch.add_leaf(*line)
+    elif line == '{':
+        # TODO: is stipping these opening quotes okay to do in parse_line?
+        continue
+    elif line == '}':
+        # Closing branch, go back to parent
+        current_branch = current_branch.parent
+    else:
+        current_branch = Branch(name=line, parent=current_branch)
+
 with open('clean_gameinfo.txt', 'w') as f:
-    f.write('\n'.join(map(str, parsed_lines)))
+    f.writelines(root_branch.serialize())
